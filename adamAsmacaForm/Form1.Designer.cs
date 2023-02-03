5310841ab9ccd961ece949d7e07737c57986c692
isildenizozturk <isildenizozturk@gmail.com>
2023-02-03 20:49:50 +0300

    Add project files.

---
+++ adamAsmacaForm/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace adamAsmacaForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.yanlisTahminlbl = new System.Windows.Forms.Label();
+            this.adamAsmacaWronglbl = new System.Windows.Forms.Label();
+            this.adamAsmacaSonuclbl = new System.Windows.Forms.Label();
+            this.adamAsmacaBosluklbl = new System.Windows.Forms.Label();
+            this.label82 = new System.Windows.Forms.Label();
+            this.adamAsmacaTahminbtn = new System.Windows.Forms.Button();
+            this.adamAsmacaHarftxt = new System.Windows.Forms.TextBox();
+            this.adamAsmacaHaklbl = new System.Windows.Forms.Label();
+            this.kalanTahminYazılbl = new System.Windows.Forms.Label();
+            this.harfGirlbl = new System.Windows.Forms.Label();
+            this.oyunuBaslatbtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnTekrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // yanlisTahminlbl
+            // 
+            this.yanlisTahminlbl.AutoSize = true;
+            this.yanlisTahminlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.yanlisTahminlbl.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.yanlisTahminlbl.Location = new System.Drawing.Point(80, 481);
+            this.yanlisTahminlbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.yanlisTahminlbl.Name = "yanlisTahminlbl";
+            this.yanlisTahminlbl.Size = new System.Drawing.Size(159, 22);
+            this.yanlisTahminlbl.TabIndex = 58;
+            this.yanlisTahminlbl.Text = "Yanlış tahminler:";
+            this.yanlisTahminlbl.Visible = false;
+            // 
+            // adamAsmacaWronglbl
+            // 
+            this.adamAsmacaWronglbl.AutoSize = true;
+            this.adamAsmacaWronglbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.adamAsmacaWronglbl.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.adamAsmacaWronglbl.Location = new System.Drawing.Point(361, 481);
+            this.adamAsmacaWronglbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.adamAsmacaWronglbl.Name = "adamAsmacaWronglbl";
+            this.adamAsmacaWronglbl.Size = new System.Drawing.Size(0, 22);
+            this.adamAsmacaWronglbl.TabIndex = 57;
+            this.adamAsmacaWronglbl.Visible = false;
+            // 
+            // adamAsmacaSonuclbl
+            // 
+            this.adamAsmacaSonuclbl.AutoSize = true;
+            this.adamAsmacaSonuclbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.adamAsmacaSonuclbl.ForeColor = System.Drawing.Color.Maroon;
+            this.adamAsmacaSonuclbl.Location = new System.Drawing.Point(633, 481);
+            this.adamAsmacaSonuclbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.adamAsmacaSonuclbl.Name = "adamAsmacaSonuclbl";
+            this.adamAsmacaSonuclbl.Size = new System.Drawing.Size(0, 24);
+            this.adamAsmacaSonuclbl.TabIndex = 56;
+            this.adamAsmacaSonuclbl.Visible = false;
+            // 
+            // adamAsmacaBosluklbl
+            // 
+            this.adamAsmacaBosluklbl.AutoSize = true;
+            this.adamAsmacaBosluklbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.adamAsmacaBosluklbl.Location = new System.Drawing.Point(125, 192);
+            this.adamAsmacaBosluklbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.adamAsmacaBosluklbl.Name = "adamAsmacaBosluklbl";
+            this.adamAsmacaBosluklbl.Size = new System.Drawing.Size(0, 26);
+            this.adamAsmacaBosluklbl.TabIndex = 55;
+            this.adamAsmacaBosluklbl.Visible = false;
+            // 
+            // label82
+            // 
+            this.label82.AutoSize = true;
+            this.label82.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label82.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label82.Location = new System.Drawing.Point(227, 11);
+            this.label82.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label82.Name = "label82";
+            this.label82.Size = new System.Drawing.Size(509, 26);
+            this.label82.TabIndex = 54;
+            this.label82.Text = "Premier Lig Takımları ile Adam Asmaca Oyunu";
+            // 
+            // adamAsmacaTahminbtn
+            // 
+            this.adamAsmacaTahminbtn.BackColor = System.Drawing.Color.LightSlateGray;
+            this.adamAsmacaTahminbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.adamAsmacaTahminbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.adamAsmacaTahminbtn.Location = new System.Drawing.Point(459, 325);
+            this.adamAsmacaTahminbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.adamAsmacaTahminbtn.Name = "adamAsmacaTahminbtn";
+            this.adamAsmacaTahminbtn.Size = new System.Drawing.Size(125, 36);
+            this.adamAsmacaTahminbtn.TabIndex = 53;
+            this.adamAsmacaTahminbtn.Text = "Tahmin";
+            this.adamAsmacaTahminbtn.UseVisualStyleBackColor = false;
+            this.adamAsmacaTahminbtn.Visible = false;
+            this.adamAsmacaTahminbtn.Click += new System.EventHandler(this.adamAsmacaTahminbtn_Click);
+            // 
+            // adamAsmacaHarftxt
+            // 
+            this.adamAsmacaHarftxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.adamAsmacaHarftxt.Location = new System.Drawing.Point(233, 327);
+            this.adamAsmacaHarftxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.adamAsmacaHarftxt.Name = "adamAsmacaHarftxt";
+            this.adamAsmacaHarftxt.Size = new System.Drawing.Size(159, 27);
+            this.adamAsmacaHarftxt.TabIndex = 52;
+            this.adamAsmacaHarftxt.Visible = false;
+            // 
+            // adamAsmacaHaklbl
+            // 
+            this.adamAsmacaHaklbl.AutoSize = true;
+            this.adamAsmacaHaklbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.adamAsmacaHaklbl.ForeColor = System.Drawing.Color.DarkCyan;
+            this.adamAsmacaHaklbl.Location = new System.Drawing.Point(323, 406);
+            this.adamAsmacaHaklbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.adamAsmacaHaklbl.Name = "adamAsmacaHaklbl";
+            this.adamAsmacaHaklbl.Size = new System.Drawing.Size(20, 22);
+            this.adamAsmacaHaklbl.TabIndex = 51;
+            this.adamAsmacaHaklbl.Text = "6";
+            this.adamAsmacaHaklbl.Visible = false;
+            // 
+            // kalanTahminYazılbl
+            // 
+            this.kalanTahminYazılbl.AutoSize = true;
+            this.kalanTahminYazılbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kalanTahminYazılbl.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.kalanTahminYazılbl.Location = new System.Drawing.Point(26, 406);
+            this.kalanTahminYazılbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.kalanTahminYazılbl.Name = "kalanTahminYazılbl";
+            this.kalanTahminYazılbl.Size = new System.Drawing.Size(210, 22);
+            this.kalanTahminYazılbl.TabIndex = 50;
+            this.kalanTahminYazılbl.Text = "Kalan tahmin hakkınız:";
+            this.kalanTahminYazılbl.Visible = false;
+            // 
+            // harfGirlbl
+            // 
+            this.harfGirlbl.AutoSize = true;
+            this.harfGirlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.harfGirlbl.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.harfGirlbl.Location = new System.Drawing.Point(80, 330);
+            this.harfGirlbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.harfGirlbl.Name = "harfGirlbl";
+            this.harfGirlbl.Size = new System.Drawing.Size(113, 22);
+            this.harfGirlbl.TabIndex = 49;
+            this.harfGirlbl.Text = "Harf giriniz:";
+            this.harfGirlbl.Visible = false;
+            // 
+            // oyunuBaslatbtn
+            // 
+            this.oyunuBaslatbtn.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.oyunuBaslatbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.oyunuBaslatbtn.Font = new System.Drawing.Font("Microsoft Tai Le", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.oyunuBaslatbtn.Location = new System.Drawing.Point(441, 65);
+            this.oyunuBaslatbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.oyunuBaslatbtn.Name = "oyunuBaslatbtn";
+            this.oyunuBaslatbtn.Size = new System.Drawing.Size(121, 39);
+            this.oyunuBaslatbtn.TabIndex = 59;
+            this.oyunuBaslatbtn.Text = "Başlat";
+            this.oyunuBaslatbtn.UseVisualStyleBackColor = false;
+            this.oyunuBaslatbtn.Click += new System.EventHandler(this.oyunuBaslatbtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(696, 112);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 321);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 60;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(503, 168);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(97, 96);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 61;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // btnTekrar
+            // 
+            this.btnTekrar.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnTekrar.Font = new System.Drawing.Font("Microsoft Tai Le", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTekrar.ForeColor = System.Drawing.SystemColors.Menu;
+            this.btnTekrar.Location = new System.Drawing.Point(416, 65);
+            this.btnTekrar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTekrar.Name = "btnTekrar";
+            this.btnTekrar.Size = new System.Drawing.Size(184, 50);
+            this.btnTekrar.TabIndex = 62;
+            this.btnTekrar.Text = "Tekrar Oyna";
+            this.btnTekrar.UseVisualStyleBackColor = false;
+            this.btnTekrar.Visible = false;
+            this.btnTekrar.Click += new System.EventHandler(this.btnTekrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnTekrar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.oyunuBaslatbtn);
+            this.Controls.Add(this.yanlisTahminlbl);
+            this.Controls.Add(this.adamAsmacaWronglbl);
+            this.Controls.Add(this.adamAsmacaSonuclbl);
+            this.Controls.Add(this.adamAsmacaBosluklbl);
+            this.Controls.Add(this.label82);
+            this.Controls.Add(this.adamAsmacaTahminbtn);
+            this.Controls.Add(this.adamAsmacaHarftxt);
+            this.Controls.Add(this.adamAsmacaHaklbl);
+            this.Controls.Add(this.kalanTahminYazılbl);
+            this.Controls.Add(this.harfGirlbl);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label yanlisTahminlbl;
+        private System.Windows.Forms.Label adamAsmacaWronglbl;
+        private System.Windows.Forms.Label adamAsmacaSonuclbl;
+        private System.Windows.Forms.Label adamAsmacaBosluklbl;
+        private System.Windows.Forms.Label label82;
+        private System.Windows.Forms.Button adamAsmacaTahminbtn;
+        private System.Windows.Forms.TextBox adamAsmacaHarftxt;
+        private System.Windows.Forms.Label adamAsmacaHaklbl;
+        private System.Windows.Forms.Label kalanTahminYazılbl;
+        private System.Windows.Forms.Label harfGirlbl;
+        private System.Windows.Forms.Button oyunuBaslatbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnTekrar;
+    }
+}
+
---
+++ adamAsmacaForm/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adamAsmacaForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] takımlar = { "manchestercity", "manchesterunited", "liverpool","chealsea",
+                  "arsenal", "tottenham", "leicestercity", "newcastleunited", "everton" };
+        
+        string takım;
+        int kalanHarf = 0;
+        int kalanHak = 6;
+        char[] bosluk;
+        int sayac = 0;
+        bool dogruTahmin;
+
+        private void oyunuBaslatbtn_Click(object sender, EventArgs e)
+        {
+
+            #region Random Team Chosing
+
+            Random random = new Random();
+            takım = takımlar[random.Next(0, takımlar.Length - 1)];
+            kalanHarf = takım.Length;
+            pictureBox1.Image = Image.FromFile(@"..\..\images\1.png");
+
+            bosluk = new char[takım.Length];
+
+            for (int i = 0; i < takım.Length; i++)
+            {
+                bosluk[i] = '_';
+                adamAsmacaBosluklbl.Text += bosluk[i].ToString() + " ";
+            }
+            adamAsmacaHaklbl.Text = kalanHak.ToString();
+
+
+            #endregion
+
+            #region Selected Team Logo into Picturebox
+
+            switch (takım)
+            {
+                case "manchestercity":
+                    pictureBox2.Image = Image.FromFile(@"..\..\images\mancity.png");
+                    break;
+                case "manchesterunited":
+                    pictureBox2.Image = Image.FromFile(@"..\..\images\manunited.png");
+                    break;
+                case "liverpool":
+                    pictureBox2.Image = Image.FromFile(@"..\..\images\liverpool.png");
+                    break;
+                case "chealsea":
+                    pictureBox2.Image = Image.FromFile(@"..\..\images\chealsea.png");
+                    break;
+                case "arsenal":
+                    pictureBox2.Image = Image.FromFile(@"..\..\images\arsenal.png");
+                    break;
+                case "tottenham":
+                    pictureBox2.Image = Image.FromFile(@"..\..\images\tottenham.png");
+                    break;
+                case "leicestercity":
+                    pictureBox2.Image = Image.FromFile(@"..\..\images\leicester.png");
+                    break;
+                case "newcastleunited":
+                    pictureBox2.Image = Image.FromFile(@"..\..\images\newcastle.png");
+                    break;
+                case "everton":
+                    pictureBox2.Image = Image.FromFile(@"..\..\images\everton.png");
+                    break;
+                default:
+                    break;
+            }
+
+            #endregion
+
+            #region Visibility Settings
+
+            kalanTahminYazılbl.Visible = true;
+            adamAsmacaHaklbl.Visible = true;
+            adamAsmacaBosluklbl.Visible = true;
+            harfGirlbl.Visible = true;
+            adamAsmacaHarftxt.Visible = true;
+            adamAsmacaTahminbtn.Visible = true;
+            yanlisTahminlbl.Visible = true;
+            adamAsmacaWronglbl.Visible = true;
+            adamAsmacaSonuclbl.Visible = true;
+            pictureBox1.Visible = true;
+
+            #endregion
+
+
+        }
+
+        private void adamAsmacaTahminbtn_Click(object sender, EventArgs e)
+        {
+
+            if (adamAsmacaHarftxt.Text.Length == 1)
+            {
+                dogruTahmin = false;
+                char val;
+                int val2;
+
+                string cvp = adamAsmacaHarftxt.Text;
+                if (Char.TryParse(cvp, out val) && !(Int32.TryParse(cvp, out val2)))
+                {
+                    char harf = char.Parse(cvp);
+                    for (int j = 0; j < takım.Length; j++)
+                    {
+                        if (harf == takım[j])
+                        {
+                            dogruTahmin = true;
+                            bosluk[j] = harf;
+                            kalanHarf--;
+                        }
+                    }
+                    adamAsmacaBosluklbl.Text = "";
+                    for (int i = 0; i < bosluk.Length; i++)
+                    {
+                        adamAsmacaBosluklbl.Text += bosluk[i].ToString() + " ";
+                    }
+
+                    if (kalanHarf > 0)
+                    {
+                        if (dogruTahmin == false)
+                        {
+                            kalanHak--;
+                            adamAsmacaHaklbl.Text = kalanHak.ToString();
+                            adamAsmacaWronglbl.Text += harf + " ";                           
+                        }
+                        
+                        if (kalanHak == 0)
+                        {
+                            adamAsmacaSonuclbl.Text = "Hakkınız Bitti!!!";
+                            pictureBox2.Visible = true;
+                            btnTekrar.Visible = true;
+                            oyunuBaslatbtn.Visible = false;
+                            adamAsmacaBosluklbl.Text = takım;
+                            pictureBox1.Image = Image.FromFile(@"..\..\images\7.png");
+                        }
+                        else if (kalanHak == 1)
+                        {  
+                            pictureBox1.Image = Image.FromFile(@"..\..\images\6.png");
+                        }
+                        else if (kalanHak == 2)
+                        {
+                            pictureBox1.Image = Image.FromFile(@"..\..\images\5.png");
+                        }
+                        else if (kalanHak == 3)
+                        {
+                            pictureBox1.Image = Image.FromFile(@"..\..\images\4.png");
+                        }
+                        else if (kalanHak == 4)
+                        {
+                            pictureBox1.Image = Image.FromFile(@"..\..\images\3.png");
+                        }
+                        else if (kalanHak == 5)
+                        {
+                            pictureBox1.Image = Image.FromFile(@"..\..\images\2.png");
+                        }
+                    }
+                    else
+                    {
+                        adamAsmacaSonuclbl.Text = "KAZANDINIZ!!!";
+                        btnTekrar.Visible = true;
+                        oyunuBaslatbtn.Visible = false;
+                        pictureBox2.Visible = true;
+                    }
+                }
+
+                else
+                {
+                    adamAsmacaSonuclbl.Text = "Lütfen sadece harf giriniz.";
+                }
+                adamAsmacaHarftxt.Text = "";
+
+                sayac++;
+
+            }
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnTekrar.Visible = false;
+            oyunuBaslatbtn.Visible = true;
+            pictureBox2.Visible = false;
+        }
+
+        private void btnTekrar_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+        }
+    }
+}
